12aa80c6c229592cc704303576625ec40dad02a2
BEREKET <Berek.dnl@gmail.com>
2022-09-22 18:59:00 -0400

    Project stracture

---
+++ PMP/src/main/java/com/propertymanagmnetportal/pmp/PmpApplication.java
@@ -0,0 +1,13 @@
+package com.propertymanagmnetportal.pmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmpApplication.class, args);
+    }
+
+}
---
+++ PMP/src/test/java/com/propertymanagmnetportal/pmp/PmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertymanagmnetportal.pmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
