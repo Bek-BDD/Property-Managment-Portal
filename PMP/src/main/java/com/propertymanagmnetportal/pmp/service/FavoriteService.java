24f99a46a7f0332cc0986aff4660b1002ea5bd59
Dawit Meketa <dawitsoft@gmail.com>
2022-09-24 13:03:47 -0500

    Favorites

---
+++ PMP/src/main/java/com/propertymanagmnetportal/pmp/service/FavoriteService.java
@@ -0,0 +1,19 @@
+package com.propertymanagmnetportal.pmp.service;
+
+import com.propertymanagmnetportal.pmp.entity.Property;
+
+import java.util.List;
+
+public interface FavoriteService {
+
+    List<Property> findAll();
+
+    List<Property> getFavoriteByUserId(int id);
+
+    void addFavorite(int user_id, int property_id);
+
+    void removeFavorite(int id);
+
+    void removeAllFavorites();
+
+}
