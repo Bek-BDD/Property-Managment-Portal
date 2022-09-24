f9fb838aebc7edf1241c08a1c70a5c55d2a31275
Dawit Meketa <dawitsoft@gmail.com>
2022-09-24 13:03:47 -0500

    Favorites

--- PMP/src/main/java/com/propertymanagmnetportal/pmp/entity/Favorite.java
+++ PMP/src/main/java/com/propertymanagmnetportal/pmp/entity/Favorite.java
@@ -1,18 +1,31 @@
 package com.propertymanagmnetportal.pmp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Favorite {
-    @EmbeddedId
-   private FavoriteCompositeKey favoriteCompositeKey;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @ManyToOne
-    @MapsId("user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
-    @MapsId("property_id")
+    @JoinColumn(name = "property_id")
     private Property property;
+
+    public Favorite(User user, Property property) {
+        this.user = user;
+        this.property = property;
+    }
 
 }
