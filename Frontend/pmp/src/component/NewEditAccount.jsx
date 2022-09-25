99c1796f7338c40e9b6405fce1ac414de26823d9
Zedagem <zedshif123@gmail.com>
2022-09-25 17:52:41 -0500

    some components add by zed

---
+++ Frontend/pmp/src/component/NewEditAccount.jsx
@@ -0,0 +1,154 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import { useState } from "react";
+
+export default function () {
+  const [userState, setUserState] = useState({
+    firstname: "Zedagem",
+    lastname: "Shiferaw",
+    email: "zedshif123@gmail.com",
+    address: "100N 4th St",
+    city:"Fairfield",
+    zipcode: "52557",
+    state: "Iowa",
+  });
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log(data.get('city'));
+  }
+
+  return (
+    <div>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "#304EF2" }}>
+          <EditIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Edit your account
+        </Typography>
+      </Box>
+      <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+         >
+      <Grid
+        container
+        columnGap={3}
+        justifyContent="center"
+        sx={{ marginTop: 3 }}
+      >
+        <Grid item xs={5}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                defaultValue={userState.firstname}
+                autoComplete="given-name"
+                name="firstname"
+                fullWidth
+                id="firstName"
+                label="First Name"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                defaultValue={userState.lastname}
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastname"
+                autoComplete="family-name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                defaultValue={userState.email}
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button variant="outlined" component="label" fullWidth>
+                Upload Image
+                <input type="file" hidden name="images" />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={5}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                defaultValue={userState.address}
+                id="address"
+                name="address"
+                label="Address line "
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                defaultValue={userState.city}
+                id="city"
+                name="city"
+                label="City"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                defaultValue={userState.state}
+                id="state"
+                name="state"
+                label="State/Province/Region"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                defaultValue={userState.zipcode}
+                id="zip"
+                name="zip"
+                label="Zip / Postal code"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Edit
+        </Button>
+        </Box>
+      </Box>
+    </div>
+  );
+}
--- Frontend/pmp/src/index.js
+++ Frontend/pmp/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
 
+export const instance = axios.create({
+     baseURL:"http://localhost:8080",
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
